f3600c8e111fd93f539e8df0090a562bc9c2b00b
ParkJuhyeong <majecty@gmail.com>
2021-01-03 10:44:06 +0900

    Try to parse s expression using chicken

--- impls/chicken/lib/reader.scm
+++ impls/chicken/lib/reader.scm
@@ -1,4 +1,12 @@
 (module lib.reader (foo)
   (import scheme)
+  (import comparse)
   (define (foo)
-    (display "This is foo\n")))
+    (display "This is foo\n"))
+  (define (read-str)
+    (error "not implemented yet"))
+  (define (symbol char) ('symbol . char))
+  (define (int num) ('int . num))
+  ;; receive string and return token array
+  (define (tokenize str)
+    (error "not implemented yet")))
