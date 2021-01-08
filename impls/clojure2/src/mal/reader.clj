c2c009af3d16bccd04b8a9d933c3bea4419c36d4
ParkJuhyeong <majecty@gmail.com>
2021-01-08 10:12:19 +0900

    Use deftest in reader
    Using `is` in top level makes the tests use uninitialized var.

--- impls/clojure2/src/mal/reader.clj
+++ impls/clojure2/src/mal/reader.clj
@@ -1,7 +1,7 @@
 (ns mal.reader
   (:require [blancas.kern.core :as kern]
             [blancas.kern.lexer.basic :as lexer]
-            [clojure.test :refer [is]])
+            [clojure.test :refer [is deftest]])
   (:gen-class))
 
 (declare p-s-expr)
@@ -18,15 +18,17 @@
    (not= c \()
    (not= c \))))
 
-(is (symbol-char? \a))
-(is (not (symbol-char? \()))
+(deftest test-symbol-char?
+  (is (symbol-char? \a))
+  (is (not (symbol-char? \())))
 
 (def p-symbol-char
   (kern/<?> (kern/satisfy symbol-char?)
             "symbol character"))
 
-(is (= \a (kern/value p-symbol-char "as")))
-(is (not (kern/value p-symbol-char "(")))
+(deftest test-p-symbol-char
+  (is (= \a (kern/value p-symbol-char "as")))
+  (is (not (kern/value p-symbol-char "("))))
 
 (def p-symbol
   (->> p-symbol-char
@@ -34,16 +36,19 @@
        (kern/<+>)
        (kern/<$> #(vector :symbol %))))
 
-(is (= [:symbol "as"] (kern/value p-symbol "as")))
+(deftest test-p-symbol
+  (is (= [:symbol "as"] (kern/value p-symbol "as"))))
 
 (def p-number
   (kern/<$> #(vector :number %) lexer/dec-lit))
 
 (def p-s-expr (kern/<|> p-list p-number p-symbol))
 
-(is (= [:list [[:number 1] [:symbol "as"]]] (kern/value p-s-expr "(1 as)")))
+(deftest test-p-s-expr
+  (is (= [:list [[:number 1] [:symbol "as"]]] (kern/value p-s-expr "(1 as)"))))
 
 (defn read-str [str]
   (kern/value p-s-expr str))
 
-(is (= [:list [[:number 1] [:symbol "as"]]] (read-str "(1 as)")))
+(deftest test-read-str
+  (is (= [:list [[:number 1] [:symbol "as"]]] (read-str "(1 as)"))))
